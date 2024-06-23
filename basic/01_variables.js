bb162ee3c6e3556fe91ea467426a01bd656b846d
avnisinngh <150375257+avnisinngh@users.noreply.github.com>
2024-06-23 06:24:03 +0000

    commit done

--- basic/01_variables.js
+++ basic/01_variables.js
@@ -1,1 +1,19 @@
-console.log("Avni");
+const accountId = 12344 //cannot be changed
+let accountEmail = "a@gmail.com"
+var accountpassword = "12"
+accountCity = "Delhi"       //not prefered
+let accountstate
+
+//accountId = 12443     //not allowed
+accountEmail = "b@gmail.com"
+accountpassword = "1234"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue of block scope & functional scope
+*/
+console.table([accountId, accountEmail, accountpassword, accountCity, accountstate])
+
