7d7ff13b278a97ea87572b728ff4b65825754849
avnisinngh <150375257+avnisinngh@users.noreply.github.com>
2024-06-23 06:57:54 +0000

    commit change done

---
+++ basic/02_dataTypes.js
@@ -0,0 +1,32 @@
+//ecma ->standards
+//mdn ->for documentation
+
+
+"use strict";       //treat all js code as newer version
+
+//alert("Hello")    //we are using node js , not browser
+
+console.log(3 +
+    3
+) //code readability should be high
+
+
+let name = "Avni"
+let age = 20
+let isLogin = true
+let state;
+
+//number => 2 to power 53
+//bitInt 
+//String => " "
+//boolean => true or false
+//null => standalone value
+//undefiner => value is not assigned
+//symbol => find uniqueness 
+
+
+//object 
+
+console.log(typeof "avni")
+console.log(typeof null)    //object
+console.log(typeof undefined)   //undefined
