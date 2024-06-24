521eba9480242e75496f97687ff4456e29ba3292
avnisinngh <150375257+avnisinngh@users.noreply.github.com>
2024-06-24 14:29:35 +0000

    conversion operation

--- basic/03_conversionOperation.js
+++ basic/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = true
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber); //NaN
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); //NaN
 
 //"33" => 33
 //"33a"=> NaN
@@ -15,7 +15,7 @@ console.log(valueInNumber); //NaN
 let isLoggedIn = "avni"
 
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+//console.log(booleanisLoggedIn);
 
 //1 =>true  0=>false
 //"" =>false
@@ -23,5 +23,40 @@ console.log(booleanisLoggedIn);
 
 let someNumber = 33
 let stringsomeNumber = String(someNumber)
-console.log(stringsomeNumber);
-console.log(typeof stringsomeNumber);
+//console.log(stringsomeNumber);
+//console.log(typeof stringsomeNumber);
+
+// *************************************OPERATIONS**********************************************************************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue)
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**3)   //2^3
+//console.log(2/3)
+//console.log(2%3)
+
+let str1 = "hello"
+let str2 = " Avni"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2)
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);// everyone is treated as first conversion
+//console.log(1 + 2 + "2");
+
+//console.log(+true)
+//console.log(+"")
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
