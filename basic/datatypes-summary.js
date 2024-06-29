c84e9055225f37629c47efc7d32e55d4643acb1a
avnisinngh <150375257+avnisinngh@users.noreply.github.com>
2024-06-29 07:39:42 +0000

    commit done

--- basic/datatypes-summary.js
+++ basic/datatypes-summary.js
@@ -4,7 +4,7 @@
 const id = Symbol('123')
 const anotherid = Symbol('123')
 
-console.log(id === anotherid)
+//console.log(id === anotherid)
 
 //REFRENCE TYPE(NON PRIMITIVE)
 // Array, objects, Functions
@@ -19,3 +19,23 @@ const myfunction  = function() {
     console.log("Hello ");
 }
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
+// Stack(primitve),                 Heap(non primitive)
+
+// copy is created,                 changes are made in original, take refrence
+
+let myytname = "Avni"
+let anothername = myytname
+anothername = "AvniSingh"
+console.log(myytname)       //different vallue
+console.log(anothername)
+
+let user1 = {
+    email:"avni@gmail.com",
+    upi:"as@ybl"
+}
+
+let user2 = user1
+user2.email = "a@google.com"
+console.log(user1.email)        //same value
+console.log(user2.email)
