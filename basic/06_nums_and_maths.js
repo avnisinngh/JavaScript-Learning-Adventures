f4de2b12f2538aa6a922c8edd962075b34fae3e7
avnisinngh <150375257+avnisinngh@users.noreply.github.com>
2024-07-02 14:48:46 +0000

    math commit

---
+++ basic/06_nums_and_maths.js
@@ -0,0 +1,31 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const othernum = 123.86003
+//console.log(othernum.toPrecision(4));
+
+const hundereds = 10000000
+//console.log(hundereds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++++++++++++++   MATHS   +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//console.log(Math);
+//console.log(Math.abs(-4));
+//console.log(Math.round(4.6));
+//console.log(Math.ceil(4.2));
+//console.log(Math.floor(4.9));
+//console.log(Math.min(1,6,54,32,10));
+//console.log(Math.max(21,54,42,55,90));
+
+//console.log(Math.random());
+//console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
