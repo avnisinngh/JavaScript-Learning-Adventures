cda09b6479d590f11ae3a43b597781fce98efdf6
avnisinngh <150375257+avnisinngh@users.noreply.github.com>
2024-07-25 18:06:54 +0000

    func done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -19,5 +19,35 @@ console.log(result);    //undefined
 function loginUserMessage(username) {
     return `${username} just logged in`
 }
-console.log(loginUserMessage("Avni"));
-console.log(loginUserMessage());    //undefined
+//console.log(loginUserMessage("Avni"));
+//console.log(loginUserMessage());    //undefined
+
+
+function calculateCartPrice(val1, val2, ...num1) {  //rest operator
+    return num1
+}
+console.log(calculateCartPrice(200,500,700));
+
+
+//object
+const user = {
+    username: "Avni",
+    price: 200
+}
+
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+//handleObject(user)
+handleObject({
+    username:"sam",
+    price: 199
+})
+
+const myNewArray = [200,300,400,500]
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
