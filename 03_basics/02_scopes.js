c2383409ee4ae785eccf8e72e1301d488ba7ca38
avnisinngh <150375257+avnisinngh@users.noreply.github.com>
2024-07-26 10:22:52 +0000

    scope done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,15 +8,51 @@ if(true) {
     let a = 10              //block scope
     const b = 20
     //c = 30
-    console.log("inner:", a);
+    //console.log("inner:", a);
 }
 
 for(let i = 0; i< Array.length;i++) {
-    const elemnt = array[i];
+   // const elemnt = array[i];
 }
 
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);     //it will be printed
 
+function one(){
+    const username = "Avni"
+
+    function two() {
+        const website = "youtuve"
+        console.log(username);
+    }
+    //console.log(website);
+
+    //two()
+}
+//one()
+
+if(true) {
+    const username = "avni"
+    if(username === "avni") {
+        const website = " youtube"
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+
+//console.log(username);
+
+//++++++++++++++++++++++++++++++++++ intersting ++++++++++++++++++++++
+
+console.log(addone(5));   // no error
+function addone(num) {
+    return num + 1;
+}
+
+
+addTwo(5)       //error, hoisting
+const addTwo = function(num) {  //expression, function
+    return num + 2
+}
