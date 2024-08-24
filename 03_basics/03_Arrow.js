7117746ca7948b613df51cc64159670cbe4f83dc
avnisinngh <150375257+avnisinngh@users.noreply.github.com>
2024-08-24 01:31:58 +0000

    commit

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,38 @@
+const user = {
+    username: "Avni",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome t0 website`);
+        
+    }
+}
+
+//user.welcomeMessage()
+//user.username = "sam"
+//user.welcomeMessage()
+
+//console.log(this);
+
+//function chai(){
+ //   let username = "Avni"
+  //  console.log(this.username);
+    
+//}
+//chai()
+
+//const chai = () => {
+  //  let username = "Avni"
+    //console.log(this.username);
+    
+//}
+
+//const addTwo = (num1, num2) => {
+  //  return num1+num2
+//}
+//console.log(
+  //  addTwo(3,4);
+//);
+
+// const addTwo = (num1, num2) => num1 + num2
+ 
