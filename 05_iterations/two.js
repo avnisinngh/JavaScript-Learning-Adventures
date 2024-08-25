06361fa91061c49446e79ac745984d609218a0e9
avnisinngh <150375257+avnisinngh@users.noreply.github.com>
2024-08-25 16:15:07 +0000

    two done

---
+++ 05_iterations/two.js
@@ -0,0 +1,12 @@
+let index = 0
+while (index <= 10) {
+    //console.log(`Value pf index is ${index}`);
+    index++
+    
+}
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
