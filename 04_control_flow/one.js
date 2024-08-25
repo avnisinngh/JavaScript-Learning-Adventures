204b96236a84f9ba8f489346b3b4da854056fb4e
avnisinngh <150375257+avnisinngh@users.noreply.github.com>
2024-08-25 07:53:57 +0000

    done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,50 @@
+// if
+const isUserLoggedIn = true
+
+if(2 == "2" ) {
+//    console.log("executed"); // executed
+    
+}else {
+    
+}
+// === for strict checking of datat types 
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+//const score = 200
+//if(score > 100) {
+ //   const power = "fly"
+ //   console.log(`User power ${power}`)
+//}
+/*
+const balance = 1000
+//if(balance > 500) console.log("test"); //implicit scope, not recomeneded
+if(balance < 500) {
+    console.log("lesas than 500");
+    
+}else if(balance < 750) {
+    console.log("less than 750");
+    
+}else if(balance < 900) {
+    console.log("less than 900");
+    
+}else{
+    console.log("greater than 900");
+    
+}
+*/
+/*
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard) {
+    console.log("Allow to buy course");
+    
+}
+if(loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User Logged in");
+    
+}*/
+
